d5317ada379c7887aef08eb5a7b8352d3788ce82
SunYangzhao <SunYangzhao@hotmail.com>
2020-10-10 16:46:06 +0800

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,6 +9,12 @@ function ~ { cd $env:USERPROFILE }
 # 转到开发环境目录
 function dev { cd $env:DEV }
 # 启动本地 node 服务器
-function localserver { node C:\DEV\WEB\server.js }
+function ws { node $env:DEV\WEB\server\index.js }
+# 本地 node 服务器配置
+function wsDev { code $env:DEV\web\server\ }
 # 递归删除当前目录下所有日志文件
 function rmlog { Get-ChildItem * -Include *.log -Recurse -Force | Remove-Item }
+# 编辑代理脚本文件
+function proxy { code $env:DEV\WEB\proxy.pac }
+# 编辑 desktop.ini
+function des { code .\desktop.ini }
